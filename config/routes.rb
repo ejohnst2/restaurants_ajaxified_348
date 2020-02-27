009a2dcdb0bc951cf52c5793d04ecaeda91dfc75
elijah johnston <eli@millinno.com>
2020-02-28 09:35:16 +1100

    lecture code init

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [ :index, :show, :destroy ] do
+    resources :reviews, only: :create
+  end
 end
